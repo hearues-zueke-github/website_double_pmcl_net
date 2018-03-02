370337c4e7a7ee659105eeedbad874d18036e152
Hearues Zueke <hziko314@gmail.com>
2018-03-02 19:54:23 +0100

    some changes, not all

--- game_2048/game_2048.js
+++ game_2048/game_2048.js
@@ -14,7 +14,8 @@ let grid;
 let color_map;
 let font_color_map;
 let number_map;
-let size = 8;
+let size = 4;
+let new_numbers = 2;
 
 function get_new_grid() {
     new_grid = [];
@@ -29,8 +30,14 @@ function get_new_grid() {
     return new_grid;
 }
 
+// TODO: make a highscore
+// TODO: make a default color pallet
+// TODO: make a default nummer map
+// TODO: make button for game
+// TODO: add game over, when no more moves are possible!
 function setup() {
-    createCanvas(canvas_size, canvas_size);
+    c = createCanvas(innerWidth, innerHeight);
+    // createCanvas(canvas_size, canvas_size);
     background(0);
 
     grid = get_new_grid()
@@ -42,22 +49,26 @@ function setup() {
     let random_value = (i_min, i_max) => i_min+getRandomInt(i_max-i_min);
     var num = 2;
     for (var i = 1; i < 30; i++) {
-        var r = random_value(50, 200);
-        var g = random_value(50, 200);
-        var b = random_value(50, 200);
+        var r = 50+(i*30)%150; // random_value(50, 200);
+        var g = 50+(Math.floor(i/5)*30)%150; // random_value(50, 200);
+        var b = 50+(Math.floor(i/5/5)*30)%150; // random_value(50, 200);
         
         color_map[i] = color(r, g, b);
         font_color_map[i] = color(255-r, 255-g, 255-b); // the invert color of color_map
+        // number_map[i] = i;
         number_map[i] = num;
         num *= 2;
     }
 
-    add_one_number();
-    add_one_number();
-    add_one_number();
+    add_new_numbers(new_numbers);
 }
 
 function draw() {
+    if (c.height != innerHeight || c.width != innerWidth) {
+        // print_window_size();
+        c.size(innerWidth, innerHeight)
+    }
+
     background(0);
 
     draw_grid();
@@ -68,7 +79,31 @@ function getRandomInt(max) {
 }
 
 function draw_grid() {
-    let w = width/size;
+    let grid_w = 0;
+    let grid_w_proc = 0.8;
+
+    let offset_x = 0;
+    let offset_y = 0;
+    let w = (c.width*grid_w_proc)/size;
+    let h = (c.height*grid_w_proc)/size;
+    
+    let offset_x_back2 = 0;
+    let offset_y_back2 = 0;
+    let offset_w_back2 = 0;
+    let offset_h_back2 = 0;
+
+    if (c.width > c.height) {
+        grid_w = Math.floor(c.height*grid_w_proc);
+        offset_y = (c.height-grid_w) / 2;
+        offset_x = (c.width-c.height) / 2 + offset_y;
+        w = h;
+    } else {
+        grid_w = Math.floor(c.width*grid_w_proc);
+        offset_x = (c.width-grid_w) / 2;
+        offset_y = (c.height-c.width) / 2 + offset_x;
+        h = w;
+    }
+
     stroke(255);
     strokeWeight(2);
     for (let j = 0; j < size; j++) {
@@ -81,7 +116,7 @@ function draw_grid() {
                 noFill();
             }
 
-            rect(i*w, j*w, w, w);
+            rect(offset_x+i*w, offset_y+j*h, w, h);
         }
     }
     
@@ -94,42 +129,41 @@ function draw_grid() {
             if (val != 0) {
                 fill(font_color_map[val]);
                 // fill(255, 255, 255);
-                text(""+number_map[val], w/2+w*i, w/2+w*j);
+                text(""+number_map[val], offset_x+w/2+w*i, offset_y+h/2+h*j);
                 // text(""+grid[j][i], w/2+w*i, w/2+w*j);
             }
         }
     }
 }
 
-function add_one_number() {
-    // First check if grid is full!
-    var is_full = true;
+function add_new_numbers(size_new_numbers) {
+    var free_spaces = 0;
     for (var j = 0; j < size; j++) {
         for (var i = 0; i < size; i++) {
             if (grid[j][i] == 0) {
-                is_full = false;
-                break;
+                free_spaces++;
+                if (free_spaces >= size_new_numbers) {
+                    break;
+                }
             }
         }
-        if (!is_full) {
+        if (free_spaces >= size_new_numbers) {
             break;
         }
     }
 
-    if (is_full) {
-        return;
+    for (var k = 0; k < free_spaces; k++) {
+        var i;
+        var j;
+        do {
+            i = getRandomInt(size);
+            j = getRandomInt(size);
+        } while (grid[j][i] != 0);
+
+        let r = Math.random();
+        grid[j][i] = r < 0.5 ? 1 : 2;
     }
-
-    var i;
-    var j;
-    do {
-        i = getRandomInt(size);
-        j = getRandomInt(size);
-
-    } while (grid[j][i] != 0);
-
-    let r = Math.random();
-    grid[j][i] = r < 0.3333 ? 1 : r < 0.6666 ? 2 : 3;
+    // grid[j][i] = r < 0.3333 ? 1 : r < 0.6666 ? 2 : 3;
 }
 
 function check_if_grid_equal(grid1, grid2) {
@@ -144,7 +178,7 @@ function check_if_grid_equal(grid1, grid2) {
     return true;
 }
 
-function move_right(grid) {
+function move_numbers_right(grid) {
     var moved_grid = get_new_grid();
 
     for (var j = 0; j < size; j++) {
@@ -176,7 +210,7 @@ function move_right(grid) {
     return moved_grid;
 }
 
-function move_left(grid) {
+function move_numbers_left(grid) {
     var moved_grid = get_new_grid();
 
     for (var j = 0; j < size; j++) {
@@ -208,7 +242,7 @@ function move_left(grid) {
     return moved_grid;
 }
 
-function move_down(grid) {
+function move_numbers_down(grid) {
     var moved_grid = get_new_grid();
 
     for (var i = 0; i < size; i++) {
@@ -240,7 +274,7 @@ function move_down(grid) {
     return moved_grid;
 }
 
-function move_up(grid) {
+function move_numbers_up(grid) {
     var moved_grid = get_new_grid();
 
     for (var i = 0; i < size; i++) {
@@ -276,22 +310,20 @@ function keyPressed() {
   var moved_grid;
   var has_moved = false;
   if (keyCode === LEFT_ARROW) {
-    moved_grid = move_left(grid);
+    moved_grid = move_numbers_left(grid);
   } else if (keyCode === RIGHT_ARROW) {
-    moved_grid = move_right(grid);
+    moved_grid = move_numbers_right(grid);
   } else if (keyCode === UP_ARROW) {
-    moved_grid = move_up(grid);
+    moved_grid = move_numbers_up(grid);
   } else if (keyCode === DOWN_ARROW) {
-    moved_grid = move_down(grid);
+    moved_grid = move_numbers_down(grid);
   }
 
   has_moved = !check_if_grid_equal(moved_grid, grid)
 
   if (has_moved) {
     grid = moved_grid;
-    add_one_number();
-    add_one_number();
-    add_one_number();
+    add_new_numbers(new_numbers);
   }
 }
 
