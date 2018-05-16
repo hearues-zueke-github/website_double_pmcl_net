db7c4474f28904cc55485bbc91c2754e8aebfdaf
Hearues Zueke <hziko314@gmail.com>
2018-05-16 21:16:04 +0200

    added old stuff

---
+++ book_of_pi/code.js
@@ -0,0 +1,13 @@
+var canv;
+
+function setup() {
+    frameRate(60);
+    canv = createCanvas(innerWidth, innerHeight);
+}
+
+function draw() {
+    background(0);
+    
+    fill(255);
+    rect(20, 40, 50, 100)
+}
