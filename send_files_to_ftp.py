796bcd9cdde123fe2df5c7ffb4f8f9683b88db35
double_pmcl <double-pmcl@double-pmcl.net>
2019-01-23 10:52:27 +0100

    fixed the new site, something at least

--- send_files_to_ftp.py
+++ send_files_to_ftp.py
@@ -5,6 +5,8 @@ import gzip
 import os
 import sys
 
+from utils import folders_dict
+
 remote_root_dir = "/www/"
 root_dir_path = os.path.abspath(os.path.dirname(__file__))+"/"
 
@@ -19,24 +21,29 @@ if __name__ == "__main__":
     pwd = data["pwd"]
     hst_adr = data["hst_adr"]
 
-    ftp_command = """
-lftp -e \"
-put -O {remote_root_dir} {root_dir_path}index.html;
-put -O {remote_root_dir} {root_dir_path}math.html;
-put -O {remote_root_dir} {root_dir_path}numeric.html;
-put -O {remote_root_dir} {root_dir_path}programming.html;
-put -O {remote_root_dir} {root_dir_path}cryptography.html;
-put -O {remote_root_dir} {root_dir_path}mystyle.css;
-put -O {remote_root_dir} {root_dir_path}nav_links.html;
-put -O {remote_root_dir} {root_dir_path}footer_links.html;
-put -O {remote_root_dir} {root_dir_path}site_viewer_counter.html;
-put -O {remote_root_dir} {root_dir_path}table_footer.html;
-bye\" -u {usr},{pwd} {hst_adr}""".format(
-        usr=usr, pwd=pwd, hst_adr=hst_adr,
-        root_dir_path=root_dir_path,
-        remote_root_dir=remote_root_dir).replace("\n", "")
+    put_template = "put -O {remote_root_dir}{{next_folder}} {root_dir_path}{{next_folder}}{{file_name}};".format(
+        remote_root_dir=remote_root_dir, root_dir_path=root_dir_path)
 
-    # print("ftp_command:\n{}".format(ftp_command))        
+    def get_next_put_commands(dct):
+        next_folder = dct["next_folder"]
+        file_names = dct["file_names"]
+
+        put_commands = ""
+        for file_name in file_names:
+            put_commands += put_template.format(next_folder=next_folder, file_name=file_name)
+
+        return put_commands
+
+    put_commands_whole = ""
+
+    # put_commands_whole += get_next_put_commands(folders_dict["root_folder"])
+    put_commands_whole += get_next_put_commands(folders_dict["startbootstrap_clean_blog_gh_pages"])
+
+    ftp_command = "lftp -e \"{put_commands_whole}bye\" -u {usr},{pwd} {hst_adr}".format(
+        put_commands_whole=put_commands_whole,
+        usr=usr,
+        pwd=pwd,
+        hst_adr=hst_adr)        
 
     print("Execute command!")
     os.system(ftp_command)
---
+++ utils.py
@@ -0,0 +1,29 @@
+folders_dict = {
+    "root_folder": {
+        "next_folder": "",
+        "file_names": [
+            "index.html",
+            "math.html",
+            "numeric.html",
+            "programming.html",
+            "cryptography.html",
+            "mystyle.css",
+            "nav_links.html",
+            "footer_links.html",
+            "site_viewer_counter.html",
+            "table_footer.html",
+        ]
+    },
+    "startbootstrap_clean_blog_gh_pages": {
+        "next_folder": "startbootstrap_clean_blog_gh_pages/",
+        "file_names": [
+            "index.html",
+            "text.html",
+            "about.html",
+            "post.html",
+            "contact.html",
+            "navigation_content.html",
+            "footer_content.html",
+        ]
+    },
+}
